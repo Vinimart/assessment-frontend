1c3243aeba67b38d1582c3e8ee13d491ba936318
Vinicius Martins <vinimartdev@gmail.com>
2020-10-02 22:45:24 -0300

    gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const babel = require('gulp-babel');
 sass.compiler = require("node-sass");
 
 gulp.task("sass", function () {
@@ -9,8 +10,16 @@ gulp.task("sass", function () {
 		.src("./public/src/style/**/*.scss")
 		.pipe(sass.sync().on("error", sass.logError))
 		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-		.pipe(gulp.dest("./public/src/style/css"));
+		.pipe(gulp.dest("./public/dist/style"));
 });
+ 
+gulp.task('default', () =>
+    gulp.src('D:\\Documents\\Projetos\\assessment-frontend\\public\\src\\script\\*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./public/dist/script'))
+);
 
 gulp.task("watch", function () {
 	gulp.watch("./public/src/style/**/*.scss", gulp.series("sass"));
